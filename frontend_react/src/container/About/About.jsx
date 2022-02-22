d2a2b383f64f5b68630aa73d540d6870646cb9de
yuhariermy <yuhariermy@gmail.com>
2022-02-22 09:29:34 +0700

    adding style for about jsx

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,16 +2,29 @@ import React, { useState, useEffect } from "react";
 
 import { motion } from "framer-motion";
 
+import { images } from "../../constants";
 import "./About.scss";
 
 const abouts = [
   {
     title: "Web Application Developer",
     description: "I am a good developer",
+    imgUrl: images.about01,
   },
   {
     title: "Back End Developer",
     description: "Enthusiast and making a several API",
+    imgUrl: images.about02,
+  },
+  {
+    title: "Title 3",
+    description: "Title 3's description",
+    imgUrl: images.about03,
+  },
+  {
+    title: "Title 4",
+    description: "Title 4's description",
+    imgUrl: images.about04,
   },
 ];
 
@@ -31,9 +44,9 @@ const About = () => {
         {abouts.map((about, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
-            whileInHover={{ scale: 1.1 }}
+            whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: "tween" }}
-            className="app__profiles-item"
+            className="app__profile-item"
             key={about.title + index}
           >
             <img src={about.imgUrl} alt={about.title} />
