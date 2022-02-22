0bd554beb3f55a7fa9d1e9439d70a0392158bf05
yuhariermy <yuhariermy@gmail.com>
2022-02-22 09:11:09 +0700

    fixing about jsx

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { motion } from "framer-motion";
+
 import "./About.scss";
 
+const abouts = [
+  {
+    title: "Web Application Developer",
+    description: "I am a good developer",
+  },
+  {
+    title: "Back End Developer",
+    description: "Enthusiast and making a several API",
+  },
+];
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    // empty notation is for
+    <>
+      <h2 className="head-text">
+        I Know That
+        <span>Good Designs</span>
+        <br />
+        means
+        <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileInHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profiles-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <h2 className="p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </h2>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
