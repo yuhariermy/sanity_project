c7092094af075d2c75a40ee56dcd4b9bd7956ce0
yuhariermy <yuhariermy@gmail.com>
2022-02-23 20:29:25 +0700

    adding testimonials

--- frontend_react/src/container/Testimonials/Testimonials.jsx
+++ frontend_react/src/container/Testimonials/Testimonials.jsx
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import { urlFor, client } from "../../client.js";
+import { motion } from "framer-motion";
+
 import "./Testimonials.scss";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState([]);
+
+  useEffect(() => {
+    const testimonialsQuery = '*[_type == "testimonials"]';
+    const brandsQuery = '*[_type == "brands"]';
+
+    client.fetch(testimonialsQuery).then((data) => {
+      setBrands(data);
+    });
+
+    client.fetch(brandsQuery).then((data) => {
+      setTestimonials(data);
+    });
+
+    client.fetch(currentIndex).then((data) => {
+      setCurrentIndex(data);
+    });
+  }, []);
+
+  const test = testimonials[currentIndex];
+
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img src={urlFor(test.imgUrl)} alt={test.name} />
+            <div className="app__testimonial-content">
+              <p className="p-text">{test.feedback}</p>
+              <div>
+                <h4 className="bold-text">{test.name}</h4>
+                <h5 className="p-text">{test.company}</h5>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
-export default Testimonials;
+export default AppWrap(
+  MotionWrap(Testimonials, "app__testimonials"),
+  "testimonials",
+  "app__primarybg"
+);
