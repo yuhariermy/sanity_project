009be0e18dae97e952f72b1aa77fdb5f2b99a036
yuhariermy <yuhariermy@gmail.com>
2022-02-23 08:32:30 +0700

    final about

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,40 +1,22 @@
 import React, { useState, useEffect } from "react";
-// import React from "react";
-import { motion } from "framer-motion";
 
+import { motion } from "framer-motion";
 import { AppWrap, MotionWrap } from "../../wrapper";
-import "./About.scss";
-import { images } from "../../constants";
-// import { urlFor } from "../../client";
 import { urlFor, client } from "../../client";
 
-const abouts = [
-  {
-    title: "Web Application Development",
-    description: "Making a Great Website",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Back End Developer",
-    description: "A Great Website Based On The Fantastic Back End",
-    imgUrl: images.about02,
-  },
-  {
-    title: "Laravel Developer",
-    description: "Making a Great Website",
-    imgUrl: images.about03,
-  },
-];
+import "./About.scss";
 
 const About = () => {
-  // const [abouts, setAbouts] = useState([]);
-  // useEffect(() => {
-  //   const query = '*[_type == "abouts"]';
+  const [abouts, setAbouts] = useState([]);
+  // const [abouts, setAbouts] = React.useState([]);
 
-  //   client.fetch(query).then((data) => {
-  //     setAbouts(data);
-  //   });
-  // }, []);
+  useEffect(() => {
+    // React.useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
 
   return (
     <>
@@ -52,9 +34,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            {/* <img src={urlFor(about.imgUrl)} alt={about.title} /> */}
-            <img src={about.imgUrl} alt={about.title} />
-            {/* <img src={urlFor(about.imgUrl)} alt={about.title} /> */}
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
