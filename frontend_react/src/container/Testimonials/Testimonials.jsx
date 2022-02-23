06400360a4d7a5f958e568be8dc50a538a58f710
yuhariermy <yuhariermy@gmail.com>
2022-02-23 21:12:26 +0700

    fixing testimonials js

--- frontend_react/src/container/Testimonials/Testimonials.jsx
+++ frontend_react/src/container/Testimonials/Testimonials.jsx
@@ -9,22 +9,20 @@ import "./Testimonials.scss";
 const Testimonials = () => {
   const [brands, setBrands] = useState([]);
   const [testimonials, setTestimonials] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClick = (index) => setCurrentIndex(index);
 
   useEffect(() => {
     const testimonialsQuery = '*[_type == "testimonials"]';
     const brandsQuery = '*[_type == "brands"]';
 
     client.fetch(testimonialsQuery).then((data) => {
-      setBrands(data);
-    });
-
-    client.fetch(brandsQuery).then((data) => {
       setTestimonials(data);
     });
 
-    client.fetch(currentIndex).then((data) => {
-      setCurrentIndex(data);
+    client.fetch(brandsQuery).then((data) => {
+      setBrands(data);
     });
   }, []);
 
@@ -42,6 +40,31 @@ const Testimonials = () => {
                 <h4 className="bold-text">{test.name}</h4>
                 <h5 className="p-text">{test.company}</h5>
               </div>
+            </div>
+          </div>
+
+          <div className="app__testimonial-btns app__flex">
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(currentIndex === 0)
+                  ? testimonials.length - 1
+                  : currentIndex - 1
+              }
+            >
+              <HiChevronLeft />
+            </div>
+          </div>
+          <div className="app__testimonial-btns app__flex">
+            <div
+              className="app__flex"
+              onClick={() =>
+                handleClick(currentIndex === testimonials.length - 1)
+                  ? 0
+                  : currentIndex + 1
+              }
+            >
+              <HiChevronRight />
             </div>
           </div>
         </>
