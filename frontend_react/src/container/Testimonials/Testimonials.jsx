c7936f1e2d84d01959e45ed96f2131a4dfa21b78
yuhariermy <yuhariermy@gmail.com>
2022-02-23 21:24:37 +0700

    final testimonials js

--- frontend_react/src/container/Testimonials/Testimonials.jsx
+++ frontend_react/src/container/Testimonials/Testimonials.jsx
@@ -47,25 +47,38 @@ const Testimonials = () => {
             <div
               className="app__flex"
               onClick={() =>
-                handleClick(currentIndex === 0)
-                  ? testimonials.length - 1
-                  : currentIndex - 1
+                handleClick(
+                  currentIndex === 0
+                    ? testimonials.length - 1
+                    : currentIndex - 1
+                )
               }
             >
               <HiChevronLeft />
             </div>
-          </div>
-          <div className="app__testimonial-btns app__flex">
             <div
               className="app__flex"
               onClick={() =>
-                handleClick(currentIndex === testimonials.length - 1)
-                  ? 0
-                  : currentIndex + 1
+                handleClick(
+                  currentIndex === testimonials.length - 1
+                    ? 0
+                    : currentIndex + 1
+                )
               }
             >
               <HiChevronRight />
             </div>
+          </div>
+          <div className="app__testimonial-brands app__flex">
+            {brands.map((brand) => (
+              <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 0.5, type: "tween" }}
+                key={brand._id}
+              >
+                <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+              </motion.div>
+            ))}
           </div>
         </>
       )}
