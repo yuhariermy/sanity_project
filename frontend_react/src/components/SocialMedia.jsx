f016d30e7b2922b5ea277570eba0a9dddeedae58
yuhariermy <yuhariermy@gmail.com>
2022-02-24 09:21:42 +0700

    final touch

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { FaFacebookF, FaLinkedin } from "react-icons/fa";
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <a href="https://twitter.com">
-        <BsTwitter />
+      <a
+        href="https://www.linkedin.com/in/ermy-yuhari-bbb310148/"
+        target="_blank"
+      >
+        <FaLinkedin />
       </a>
     </div>
     <div>
-      <a href="https://facebook.com">
+      <a target="_blank" href="https://www.facebook.com/ermie.harie/">
         <FaFacebookF />
       </a>
     </div>
     <div>
-      <a href="https://instagram.com">
+      <a href="https://www.instagram.com/ermieharie/" target="_blank">
         <BsInstagram />
       </a>
     </div>
