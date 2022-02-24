72887fd4ec8cece0e7fff1a3dab0c050def766ca
yuhariermy <yuhariermy@gmail.com>
2022-02-24 13:40:04 +0700

    fixing skills

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -35,7 +35,7 @@ const Skills = () => {
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="app__skills-item app__flex"
-              key={skill}
+              key={skill.name}
             >
               <div
                 className="app__flex"
@@ -52,14 +52,12 @@ const Skills = () => {
             .slice()
             .sort((a, b) => (a.year < b.year ? 1 : b.year < a.year ? -1 : 0))
             .map((experience, index) => (
-              <motion.div
-                className="app__skills-exp-item"
-                key={experience.year + index}
-              >
+              <motion.div className="app__skills-exp-item" key={experience._id}>
                 <div className="app__skills-exp-year">
                   <p className="bold-text">{experience.year}</p>
                 </div>
                 <motion.div className="app__skills-exp-works">
+                  {console.log("here", experience)}
                   {experience.works.map((work, index) => (
                     <>
                       <motion.div
@@ -67,14 +65,14 @@ const Skills = () => {
                         transition={{ duration: 0.5 }}
                         className="app__skills-exp-work"
                         data-tip
-                        data-for={work.company}
-                        key={work.company}
+                        data-for={work._key}
+                        key={work._key}
                       >
                         <h4 className="bold-text">{work.name}</h4>
                         <p className="p-text">{work.company}</p>
                       </motion.div>
                       <ReactTooltip
-                        id={work.company}
+                        id={work._key}
                         effect="solid"
                         arrowColor="#fff"
                         className="skills-tooltip"
